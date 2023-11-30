7f05d345d9392e27b3292f94947cd9d06c713ccf
Abdullah Hussein <abdullabale11@gmail.com>
2023-11-30 20:33:12 +0300

    refactor: create a components for the todos.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,25 @@
 import "./App.css";
-import axios from "axios";
 import { useEffect, useState } from "react";
 
-import { MoreHorizontalIcon } from "lucide-react";
-import { TrashIcon } from "lucide-react";
-import { Star } from "lucide-react";
+import axios from "axios";
+
 import { Oval } from "react-loader-spinner";
 
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import MyDialog from "./components/ui/myDialog";
+import MyDialog from "./components/MyDialog";
+import Todo from "./components/Todo";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [isLoaderVisible, setIsLoaderVisible] = useState(true);
 
-  const pinnedTodos = todos.filter((todo) => todo.pinned == true);
-  const unPinnedTodos = todos.filter((todo) => todo.pinned != true);
+  type Todo = {
+    todo_id: number;
+    description: string;
+    pinned: boolean;
+  };
+
+  const pinnedTodos = todos.filter((todo: Todo) => todo.pinned == true);
+  // const unPinnedTodos = todos.filter((todo) => todo.pinned != true);
 
   const fetchData = async () => {
     const result = await axios
@@ -53,146 +53,9 @@ function App() {
             strokeWidth={5}
             strokeWidthSecondary={5}
           />
-          {pinnedTodos.map(
-            (todo: {
-              todo_id: number;
-              description: string;
-              pinned: boolean;
-            }) => (
-              <div
-                key={todo.todo_id}
-                className="border-[0.2px] p-3 rounded-xl flex items-start justify-between"
-              >
-                <div className="flex gap-2">
-                  <input
-                    className="accent-black self-start mt-[5.5px]"
-                    type="checkbox"
-                    name="todo"
-                    id={String(todo.todo_id)}
-                  />
-                  <label
-                    htmlFor={String(todo.todo_id)}
-                    className="p-0 flex  leading-6"
-                    key={todo.todo_id}
-                  >
-                    {todo.description}
-                  </label>
-                </div>
-                <div className="flex gap-1 mt-[5px] self-start">
-                  <Star
-                    size={15}
-                    className={[
-                      " transition-colors duration-300",
-                      todo.pinned
-                        ? "text-yellow-500 fill-yellow-500  hover:text-yellow-600 hover:fill-yellow-600"
-                        : "text-slate-500  hover:text-slate-900",
-                    ].join(" ")}
-                    onClick={async () => {
-                      await axios.put(
-                        `http://localhost:5000/todos/${todo.todo_id}`,
-                        {
-                          description: todo.description,
-                          pinned: !todo.pinned,
-                        }
-                      );
-                    }}
-                  />
-                  <Popover>
-                    <PopoverTrigger>
-                      <MoreHorizontalIcon
-                        size={16}
-                        className=" text-slate-500 hover:text-slate-900 transition-colors duration-300 "
-                      />
-                    </PopoverTrigger>
-                    <PopoverContent className="flex flex-col  w-fit p-2 rounded-xl">
-                      <MyDialog title="Edit" method="put" />
-                      <div
-                        onClick={async () => {
-                          await axios.delete(
-                            `http://localhost:5000/todos/${todo.todo_id}`
-                          );
-                        }}
-                        className="flex text-slate-600 hover:text-slate-900 cursor-pointer items-center justify-start p-2 gap-2 hover:bg-slate-50 rounded-lg"
-                      >
-                        <TrashIcon size={16} />
-                        <p>Delete</p>
-                      </div>
-                    </PopoverContent>
-                  </Popover>
-                </div>
-              </div>
-            )
-          )}
-          {unPinnedTodos.map(
-            (todo: {
-              todo_id: number;
-              description: string;
-              pinned: boolean;
-            }) => (
-              <div
-                key={todo.todo_id}
-                className="border-[0.2px] p-3 rounded-xl flex items-start justify-between"
-              >
-                <div className="flex gap-2">
-                  <input
-                    className="accent-black self-start mt-[5.5px]"
-                    type="checkbox"
-                    name="todo"
-                    id={String(todo.todo_id)}
-                  />
-                  <label
-                    htmlFor={String(todo.todo_id)}
-                    className="p-0 flex  leading-6"
-                    key={todo.todo_id}
-                  >
-                    {todo.description}
-                  </label>
-                </div>
-                <div className="flex gap-1 mt-[5px] self-start">
-                  <Star
-                    size={15}
-                    className={[
-                      " transition-colors duration-300",
-                      todo.pinned
-                        ? "text-yellow-500 fill-yellow-500  hover:text-yellow-600 hover:fill-yellow-600"
-                        : "text-slate-500  hover:text-slate-900",
-                    ].join(" ")}
-                    onClick={async () => {
-                      await axios.put(
-                        `http://localhost:5000/todos/${todo.todo_id}`,
-                        {
-                          description: todo.description,
-                          pinned: !todo.pinned,
-                        }
-                      );
-                    }}
-                  />
-                  <Popover>
-                    <PopoverTrigger>
-                      <MoreHorizontalIcon
-                        size={16}
-                        className=" text-slate-500 hover:text-slate-900 transition-colors duration-300 "
-                      />
-                    </PopoverTrigger>
-                    <PopoverContent className="flex flex-col  w-fit p-2 rounded-xl">
-                      <MyDialog title="Edit" method="put" />
-                      <div
-                        onClick={async () => {
-                          await axios.delete(
-                            `http://localhost:5000/todos/${todo.todo_id}`
-                          );
-                        }}
-                        className="flex text-slate-600 hover:text-slate-900 cursor-pointer items-center p-2 gap-2 hover:bg-slate-50 rounded-lg"
-                      >
-                        <TrashIcon size={16} />
-                        <p>Delete</p>
-                      </div>
-                    </PopoverContent>
-                  </Popover>
-                </div>
-              </div>
-            )
-          )}
+          {todos.map((todo: Todo) => (
+            <Todo todo={todo} />
+          ))}
         </div>
       </div>
     </div>
---
+++ client/src/components/Todo.tsx
@@ -0,0 +1,93 @@
+import { Star } from "lucide-react";
+import { MoreHorizontalIcon } from "lucide-react";
+import { TrashIcon } from "lucide-react";
+import axios from "axios";
+import MyDialog from "./MyDialog";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+
+type Todo = {
+  todo_id: number;
+  description: string;
+  pinned: boolean;
+};
+
+type Props = {
+  todo: Todo;
+};
+
+const Todo = (props: Props) => {
+  const [pinned, setPinned] = useState(props.todo.pinned);
+
+  return (
+    <div
+      key={props.todo.todo_id}
+      className="border-[0.2px] p-3 rounded-xl flex items-start justify-between"
+    >
+      <div className="flex gap-2">
+        <input
+          className="accent-black self-start mt-[5.5px]"
+          type="checkbox"
+          name="todo"
+          id={String(props.todo.todo_id)}
+        />
+        <label
+          htmlFor={String(props.todo.todo_id)}
+          className="p-0 flex  leading-6"
+          key={props.todo.todo_id}
+        >
+          {props.todo.description}
+        </label>
+      </div>
+      <div className="flex gap-1 mt-[5px] self-start">
+        <Star
+          size={15}
+          className={[
+            " transition-colors duration-300",
+            pinned
+              ? "text-yellow-500 fill-yellow-500  hover:text-yellow-600 hover:fill-yellow-600"
+              : "text-slate-500  hover:text-slate-900",
+          ].join(" ")}
+          onClick={async () => {
+            await axios.put(
+              `http://localhost:5000/todos/${props.todo.todo_id}`,
+              {
+                description: props.todo.description,
+                pinned: !props.todo.pinned,
+              }
+            );
+          }}
+        />
+        <Popover>
+          <PopoverTrigger>
+            <MoreHorizontalIcon
+              size={16}
+              className=" text-slate-500 hover:text-slate-900 transition-colors duration-300 "
+            />
+          </PopoverTrigger>
+          <PopoverContent className="flex flex-col  w-fit p-2 rounded-xl">
+            <MyDialog title="Edit" method="put" />
+            <div
+              onClick={async () => {
+                await axios.delete(
+                  `http://localhost:5000/todos/${props.todo.todo_id}`
+                );
+              }}
+              className="flex text-slate-600 hover:text-slate-900 cursor-pointer items-center justify-start p-2 gap-2 hover:bg-slate-50 rounded-lg"
+            >
+              <TrashIcon size={16} />
+              <p>Delete</p>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
