61c052ca7cb21d95a0397b57bdb6b530f48efac5
Abdullah Hussein <abdullabale11@gmail.com>
2023-12-12 14:27:00 +0300

    add: adding tab buttons for easier reachability

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import { Oval } from "react-loader-spinner";
 import Todo from "./Todo";
 import axios from "axios";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type TodoType = {
   todo_id: number;
@@ -18,61 +19,78 @@ type Props = {
 
 const TodoList = ({ todos, setTodos, isLoaderVisible }: Props) => {
   return (
-    <div className="flex flex-col gap-2 bg-white flex-1 rounded-2xl">
-      <h1 className="font-bold text-lg border-b-[2px] mb-2 ">todos</h1>
-      <Oval
-        height={30}
-        width={30}
-        visible={isLoaderVisible}
-        color="blue"
-        wrapperClass="flex items-center justify-center mt-20"
-        ariaLabel="oval-loading"
-        secondaryColor="gray"
-        strokeWidth={5}
-        strokeWidthSecondary={5}
-      />
-      {todos
-        .filter((todo) => todo.pinned && !todo.completed)
-        .map((todo) => (
-          <Todo
-            key={todo.todo_id}
-            todo={todo}
-            todos={todos}
-            setTodos={setTodos}
+    <Tabs defaultValue="Todos" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="Todos">Todos</TabsTrigger>
+        <TabsTrigger value="History">History</TabsTrigger>
+        <TabsContent value="Todos" className="flex flex-col gap-2 col-span-full">
+        <h1 className="font-bold text-lg border-b-[2px] mb-2 mt-4">todos</h1>
+          <Oval
+            height={30}
+            width={30}
+            visible={isLoaderVisible}
+            color="blue"
+            wrapperClass="flex items-center justify-center mt-20"
+            ariaLabel="oval-loading"
+            secondaryColor="gray"
+            strokeWidth={5}
+            strokeWidthSecondary={5}
           />
-        ))}
-      {todos
-        .filter((todo) => !todo.pinned && !todo.completed)
-        .map((todo) => (
-          <Todo
-            key={todo.todo_id}
-            todo={todo}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        ))}
-      <div className="flex justify-between border-b-[2px] mb-2 items-center mt-5"><h1 className="font-bold text-lg  ">history</h1>
-      <button className="hover:bg-red-50  m-1 text-slate-500 px-2 rounded-lg hover:text-red-500 transition-colors h-7" onClick={()=>{
-        {todos
-          .filter((todo) => todo.completed)
-          .forEach(async(todo) => {
-            setTodos(todos.filter((todo)=> !todo.completed))
-            await axios.delete(
-              `http://localhost:5000/todos/${todo.todo_id}`
-            );
-          })}
-      }}>clear all</button></div>
-      {todos
-        .filter((todo) => todo.completed)
-        .map((todo) => (
-          <Todo
-            key={todo.todo_id}
-            todo={todo}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        ))}
-    </div>
+          {todos
+            .filter((todo) => todo.pinned && !todo.completed)
+            .map((todo) => (
+              <Todo
+                key={todo.todo_id}
+                todo={todo}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            ))}
+          {todos
+            .filter((todo) => !todo.pinned && !todo.completed)
+            .map((todo) => (
+              <Todo
+                key={todo.todo_id}
+                todo={todo}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            ))}
+        </TabsContent>
+        <TabsContent value="History" className="flex flex-col w-full gap-2 col-span-full">
+          <div className="flex justify-between border-b-[2px] mt-4 mb-2 items-center ">
+            <h1 className="font-bold text-lg  ">history</h1>
+            <button
+              className="hover:bg-red-50  m-1 text-slate-500 px-2 rounded-lg hover:text-red-500 transition-colors h-7"
+              onClick={() => {
+                {
+                  todos
+                    .filter((todo) => todo.completed)
+                    .forEach(async (todo) => {
+                      setTodos(todos.filter((todo) => !todo.completed));
+                      await axios.delete(
+                        `http://localhost:5000/todos/${todo.todo_id}`
+                      );
+                    });
+                }
+              }}
+            >
+              clear all
+            </button>
+          </div>
+          {todos
+            .filter((todo) => todo.completed)
+            .map((todo) => (
+              <Todo
+                key={todo.todo_id}
+                todo={todo}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            ))}
+        </TabsContent>
+      </TabsList>
+    </Tabs>
   );
 };
 
