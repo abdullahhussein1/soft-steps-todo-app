858c01d4a6b22aa9889f46b476ee8d65487830a8
Abdullah Hussein <abdullabale11@gmail.com>
2024-01-06 18:12:49 +0300

    bug: theme provider not working properly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
 import "./App.css";
 
 import TodoApp from "./components/TodoApp";
-import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <TodoApp />
-    </ThemeProvider>
-  );
+  return <TodoApp />;
 }
 
 export default App;
--- client/src/components/TodoApp.tsx
+++ client/src/components/TodoApp.tsx
@@ -1,13 +1,9 @@
 import TodoList from "./TodoList";
-import { ModeToggle } from "./mode-toggle";
 
 const TodoApp = () => {
   return (
     <div className="h-[100dvh] flex flex-col bg-secondary justify-center items-center">
       <div className="w-full max-w-xl gap-2 flex flex-col p-5">
-        <div className="self-start">
-          <ModeToggle />
-        </div>
         <div className="container bg-background shadow-2xl shadow-gray-950/20 rounded-[30px] h-[590px] max-w-xl flex flex-col p-5">
           <TodoList />
         </div>
