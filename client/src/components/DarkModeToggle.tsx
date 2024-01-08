899fea0cafaa4e100bd8d12677790833373b1a63
Abdullah Hussein <abdullabale11@gmail.com>
2024-01-08 14:42:00 +0300

    fix: typescript type cheching for DarkModeToggle.tsx

--- client/src/components/DarkModeToggle.tsx
+++ client/src/components/DarkModeToggle.tsx
@@ -2,22 +2,30 @@ import { useEffect } from "react";
 import { useTheme } from "@/components/ThemeProvider";
 import { Switch } from "@/components/ui/switch";
 
+type Theme =
+  | "blue"
+  | "blue-dark"
+  | "red"
+  | "red-dark"
+  | "green"
+  | "green-dark"
+  | "orange"
+  | "orange-dark";
+
 export default function DarkModeToggle() {
   const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     const isDarkMode = localStorage.getItem("darkMode") === "true";
     if (isDarkMode) {
-      setTheme((prevTheme) =>
-        prevTheme.includes("-dark") ? prevTheme : prevTheme + "-dark"
-      );
+      setTheme(theme.includes("-dark") ? theme : ((theme + "-dark") as Theme));
     }
-  }, [setTheme]);
+  }, [setTheme, theme]);
 
   const toggleDarkMode = () => {
     const newTheme = theme.includes("-dark")
-      ? theme.replace("-dark", "")
-      : theme + "-dark";
+      ? (theme.replace("-dark", "") as Theme)
+      : ((theme + "-dark") as Theme);
     setTheme(newTheme);
     localStorage.setItem(
       "darkMode",
