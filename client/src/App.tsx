6af664d51a5534e7716c8954dd3058514aa7b1e8
Abdullah Hussein <abdullabale11@gmail.com>
2024-02-04 22:34:35 +0300

    refactor: delete debugging lines

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import TodoApp from "./components/TodoApp";
 import ThemeProvider from "@/components/ThemeProvider";
 
 function App() {
-  console.log(import.meta.env.VITE_API_BASE_URL);
   return (
     <ThemeProvider defaultTheme="blue" storageKey="vite-ui-theme">
       <TodoApp />
