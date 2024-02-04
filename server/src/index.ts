70a2c982cb29032666de6f2385167366fc0884c8
Abdullah Hussein <abdullabale11@gmail.com>
2024-02-04 15:01:24 +0300

    fix: todos route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import todosRoute from "./routes/api/todosRouteApi"; // Adjust the path based on your project structure
+import todosRoute from "./routes/api/todosRouteApi";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Routes
-app.use("/api/todos", todosRoute); // Mount the todosRoute under '/api/todos'
+app.use("/todos", todosRoute);
 
 // Start the server
 app.listen(PORT, () => {
