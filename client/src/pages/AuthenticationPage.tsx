bba959560af13e695c60b346dafbbab6a7763367
Abdullah Hussein <abdullabale11@gmail.com>
2024-02-11 22:23:58 +0300

    update: auth page

--- client/src/pages/AuthenticationPage.tsx
+++ client/src/pages/AuthenticationPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import supabase from "@/supabase/supabase";
+import { Sparkle } from "lucide-react";
+import { Github } from "lucide-react";
 
 const AuthenticationPage: React.FC = () => {
   supabase.auth.onAuthStateChange((event, session) => {
@@ -26,6 +28,9 @@ const AuthenticationPage: React.FC = () => {
   async function handleSignInWithGoogle() {
     const { data } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: import.meta.env.VITE_BASE_URL as string,
+      },
     });
 
     console.log(data);
@@ -33,17 +38,35 @@ const AuthenticationPage: React.FC = () => {
   async function handleSignInWithGithub() {
     const { data } = await supabase.auth.signInWithOAuth({
       provider: "github",
+      options: {
+        redirectTo: import.meta.env.VITE_BASE_URL as string,
+      },
     });
 
     console.log(data);
   }
 
   return (
-    <div className="p-5 bg-neutral-900 flex items-center  justify-center h-screen">
+    <div className="p-5 bg-neutral-900 flex items-center  justify-center flex-col gap-4 h-screen">
+      <h1 className="text-3xl font-light">
+        Welcome to <span className="font-semibold">Soft Steps</span>
+      </h1>
       <div className="bg-black max-w-xl w-full p-5 rounded-3xl">
-        <div className="flex justify-between flex-col">
-          <button onClick={handleSignInWithGoogle}>Sign in With Google</button>
-          <button onClick={handleSignInWithGithub}>Sign in With Github</button>
+        <div className="flex justify-center items-center gap-5 flex-col">
+          <button
+            className="flex gap-2 items-center rounded-xl p-3 hover:bg-neutral-900/50 transition-colors"
+            onClick={handleSignInWithGoogle}
+          >
+            <Sparkle size={18} />
+            <p>Sign in With Google</p>
+          </button>
+          <button
+            className="flex gap-2 items-center rounded-xl p-3 hover:bg-neutral-900/50 transition-colors"
+            onClick={handleSignInWithGithub}
+          >
+            <Github size={18} />
+            <p>Sign in With Github</p>
+          </button>
         </div>
       </div>
     </div>
