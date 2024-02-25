55ec590f7a743f162e53c2cf4c0bc59e797c2d04
Abdullah Hussein <abdullabale11@gmail.com>
2024-02-25 17:58:50 +0300

    update: auth page

--- client/src/pages/AuthenticationPage.tsx
+++ client/src/pages/AuthenticationPage.tsx
@@ -47,28 +47,30 @@ const AuthenticationPage: React.FC = () => {
   }
 
   return (
-    <div className="p-5 bg-black text-white flex items-center justify-center flex-col gap-10 h-screen">
-      <h1 className="text-xl self-start  font-semibold">🇵🇸 Soft Steps</h1>
-      <div className="flex flex-col self-start justify-self-start gap-1">
-        <h1 className="text-xl">Get started</h1>
-        <p className="text-sm font-thin">Create a new account</p>
-      </div>
-      <div className="bg-neutral-900/40 max-w-xl w-full p-5 rounded-3xl">
-        <div className="flex justify-center items-center gap-5 flex-col">
-          <button
-            className="flex gap-2 justify-center items-center rounded-2xl p-3 border-2 border-neutral-900 hover:bg-neutral-900 transition-colors"
-            onClick={handleSignInWithGoogle}
-          >
-            <Sparkle size={18} />
-            <p>Sign in With Google</p>
-          </button>
-          <button
-            className="flex gap-2 justify-center items-center rounded-2xl p-3 border-2 border-neutral-900 hover:bg-neutral-900 transition-colors"
-            onClick={handleSignInWithGithub}
-          >
-            <Github size={18} />
-            <p>Sign in With Github</p>
-          </button>
+    <div className="px-12 py-5 relative bg-black text-white  flex items-center justify-center flex-col gap-10 h-screen">
+      <h1 className="text-2xl self-start   font-semibold">🇵🇸 Soft Steps</h1>
+      <div className="flex flex-col w-full gap-2">
+        <div className="flex flex-col self-start justify-self-start gap-1">
+          <h1 className="text-xl">Get started</h1>
+          <p className="text-sm font-thin">Create a new account</p>
+        </div>
+        <div className="border max-w-xl w-full h-80 flex items-center justify-center p-5 rounded-3xl">
+          <div className="flex justify-center items-center gap-5 flex-col">
+            <button
+              className="flex gap-2 justify-center items-center rounded-2xl p-3 bg-neutral-900 hover:bg-neutral-800 transition-colors"
+              onClick={handleSignInWithGoogle}
+            >
+              <Sparkle size={18} />
+              <p>Sign in With Google</p>
+            </button>
+            <button
+              className="flex gap-2 justify-center items-center rounded-2xl p-3  bg-neutral-900 hover:bg-neutral-800 transition-colors"
+              onClick={handleSignInWithGithub}
+            >
+              <Github size={18} />
+              <p>Sign in With Github</p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
